c707f2e08cce6833cf598100f2a220bfeb97a279
lexkX64 <lexkX64@users.noreply.github.com>
2015-10-12 15:43:53 -0300

    Project conventions (Indent)

--- app/public/frontend/views/IndexView.js
+++ app/public/frontend/views/IndexView.js
@@ -1,39 +1,39 @@
 define(function(require) {
-	var template = require('text!frontend/templates/index.html'), 
-	ErrorHelper = require('frontend/helpers/ErrorHelper'),
-  Publicaciones = require('frontend/collections/Publicaciones'),
-	PublicacionView = require('frontend/views/PublicacionView');
+var template = require('text!frontend/templates/index.html'), 
+    ErrorHelper = require('frontend/helpers/ErrorHelper'),
+    Publicaciones = require('frontend/collections/Publicaciones'),
+    PublicacionView = require('frontend/views/PublicacionView');
 
-	return Backbone.View.extend({
-		template : _.template(template),
-		events : {
-		},
+  return Backbone.View.extend({
+    template : _.template(template),
+    events : {
+    },
 
-		render : function() {
-			this.$el.html(this.template);
-			this.collection = new Publicaciones();
-			this.listenTo(this.collection, 'reset', this.renderCollection);
+    render : function() {
+      this.$el.html(this.template);
+      this.collection = new Publicaciones();
+      this.listenTo(this.collection, 'reset', this.renderCollection);
 
-			this.collection.fetch({
-				reset : true,
-				error : function(collection, xhr, options) {
-					ErrorHelper.showError(xhr);
-				}
-			});
-			return this;
-		},
+      this.collection.fetch({
+        reset : true,
+        error : function(collection, xhr, options) {
+          ErrorHelper.showError(xhr);
+        }
+      });
+      return this;
+    },
 
     renderCollection: function() {
       this.collection.each(function(item) {
         this.renderItem(item);
       }, this);
-      
-     this.$('#publicaciones-container').pinterest_grid({
+
+      this.$('#publicaciones-container').pinterest_grid({
         no_columns: 4,
-       padding_x: 10,
-       padding_y: 10,
-       margin_bottom: 50,
-       single_column_breakpoint: 700
+        padding_x: 10,
+        padding_y: 10,
+        margin_bottom: 50,
+        single_column_breakpoint: 700
       });
     },
 
@@ -44,5 +44,5 @@ define(function(require) {
 
       this.$('#publicaciones-container').append(publicacionView.render().el);
     }
-	});
+  });
 });
