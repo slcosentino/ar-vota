4c1d920bb98ac3e6f51f6ebdcdd4c169248d128a
lexkX64 <lexkX64@users.noreply.github.com>
2015-10-18 16:29:55 -0300

    Remove old files

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,8 +4,6 @@ module.exports = function(app) {
   app.use('/api/usuarios', require('../routes/usuarios'));
   app.use('/api/admin', require('../routes/admin'));
   app.use('/api/encuestas', require('../routes/encuestas'));
-  app.use('/api/propuestas', require('../routes/propuestas'));
-  app.use('/api/quejas', require('../routes/quejas'));
   app.use('/api/images', require('../routes/images'));
   app.use('/api/publicaciones', require('../routes/publicaciones'));
 }
